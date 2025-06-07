66b30f524e679d266f4b1dc0a853d4232d9761dc
unknown <tahakhan4141@gmail.com>
2025-06-07 22:39:45 +0500

    Custom Text Added

--- lib/custom_widgets/custom_Text.dart
+++ lib/custom_widgets/custom_Text.dart
@@ -2,21 +2,27 @@ import 'package:flutter/material.dart';
 
 class NormalText extends StatelessWidget {
   final t_string;
-  final t_font;
+  final t_font_size;
   final t_color;
+  final t_fontFamily;
 
-  const NormalText({
+  NormalText({
     super.key,
     required this.t_string,
     required this.t_color,
-    required this.t_font,
+    required this.t_font_size,
+    this.t_fontFamily,
   });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       '$t_string',
-      style: TextStyle(fontSize: t_font, color: t_color),
+      style: TextStyle(
+        fontSize: t_font_size,
+        color: t_color,
+        fontFamily: t_fontFamily,
+      ),
     );
   }
 }
