c53caa0e930d34edbba5481e40d7cc7fb0aef465
unknown <tahakhan4141@gmail.com>
2025-07-21 23:58:50 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class _Liverify_Disease_detectionState
   @override
   Widget build(BuildContext context) {
     print("main Screen");
+    print("two");
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
